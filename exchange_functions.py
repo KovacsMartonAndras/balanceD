6a5efa98cadc5a66135d76b8d91f2190b310e4ad
Marci <mk@nilt.com>
2025-09-20 21:19:10 +0200

    TODO Laci added

--- exchange_functions.py
+++ exchange_functions.py
@@ -1,6 +1,5 @@
-
 def convert_to_currency(source,target,amount):
-    #TODO Implement live transaction rates, should return the amount in the target currency based on the current exhange rates
+    #TODO Laci
     if source == "HUF":
         return amount
     else:
